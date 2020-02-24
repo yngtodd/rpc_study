9960cdeaef26b3dcbdb4497b6492465d38813302
Young, Todd <youngmt1@ornl.gov>
2020-02-23 22:51:06 -0500

    Simplify path
    There was some unnecessary steps when resolving the path.

--- rpc_study/launcher.py
+++ rpc_study/launcher.py
@@ -1,6 +1,6 @@
 import argparse
 import subprocess
-from pathlib import Path, PurePath
+from pathlib import Path
 
 
 def main():
@@ -8,8 +8,8 @@ def main():
     parser.add_argument('path', metavar='FILENAME', help='path to study') 
     args = parser.parse_args()
 
-    launch_path = Path(__file__).resolve().parent
-    run_script = PurePath(launch_path).joinpath('run.sh')
+    launch_path = Path(__file__).parent
+    run_script = launch_path.joinpath('run.sh')
 
     # Use Pytorch's distributed launch
     subprocess.Popen(["/bin/bash", run_script, args.path])
